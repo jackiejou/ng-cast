99fa53ac9d6a0e36b2d6918be7bd1abde604697a
jackiejou <joujackie@gmail.com>
2017-12-16 10:30:02 -0800

    finished basic requirements

--- src/components/search.js
+++ src/components/search.js
@@ -10,6 +10,16 @@ angular.module('video-player')
       // console.log('search controller this', JSON.stringify(this));
       console.log('render search');
       // youTube.search(this.options);
+      this.searchString = '';
+      this.clickHandler = () => {
+        this.options = {
+          key: window.YOUTUBE_API_KEY,
+          max: 5,
+          query: this.searchString
+        };
+        youTube.search(this.options, this.callback);
+        this.searchString = '';
+      };
     },
     templateUrl: 'src/templates/search.html'
   });
